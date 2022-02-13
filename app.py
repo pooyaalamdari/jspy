2fad1de9002e5f656be3ec415f9ccbf79f6bc00d
pooyaghobdaialamdari <pooya.gh.alamdari@gmail.com>
2022-02-13 16:01:52 +0330

    from-import

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-import file_operation
+from file_operation import save_to_file
 
-file_operation.save_to_file('Pooya','data.txt')
+save_to_file('Alex','data.txt')
 
 #if run -> the file with data.txt name will create
 #and content in data.txt file will be Pooya
