412471173303a95faab521fb2827d66bcbafacfd
pooyaghobdaialamdari <pooya.gh.alamdari@gmail.com>
2022-02-13 14:30:51 +0330

    json python 2

--- json_imports.py
+++ json_imports.py
@@ -8,3 +8,18 @@ file_contents = json.load(file)
 file.close()
 
 print(file_contents['friends'][0])
+
+
+#convert python dictionary to json file
+
+cars = [
+    {'make':'Ford', 'model':'Fiesta'},
+    {'make':'Ford', 'model':'Focus'}
+]
+
+file = open('cars_json.txt', 'w')
+#build file and it's name is cars_json.txt
+json.dump(cars, file)
+file.close()
+
+#after run the json file will be created in the cars_json.txt
